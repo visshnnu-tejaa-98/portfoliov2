0c49913b083510e11a7112647c1876f9af729c82
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2023-04-07 00:51:54 +0530

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  function toggleDarkMode() {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`h-full w-full mx-auto py-2 text-center
+    ${darkMode ? "dark" : ""}`}
+    >
+      <h1 className="bg:red-300 dark:[#0F172A]">Visshnnu's Portfolio</h1>
+      <button onClick={toggleDarkMode} className="p-2 bg-red-500">
+        Toggle Theme
+      </button>
     </div>
   );
 }
